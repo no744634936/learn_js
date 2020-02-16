db8f9abef2056b88e2acd18f7ee3083999ae4e39
no744634936 <zhanghaifeng1123@gmail.com>
2020-02-16 18:07:34 +0900

    first commit

---
+++ master.js
@@ -0,0 +1,49 @@
+// booleans & comparisons
+console.log(true, false, 'true', 'false');
+
+// methods can return booleans
+let email = 'luigi@thenetninja.co.uk';
+let names = ['mario', 'luigi', 'toad'];
+
+let result = email.includes('@');
+let result = names.includes('luigi');
+
+console.log(result);
+
+// comparison operators
+let age = 25;
+
+console.log(age == 25);
+console.log(age == 30);
+console.log(age != 30);
+console.log(age > 20);
+console.log(age < 20);
+console.log(age <= 25);
+console.log(age >= 25);
+
+let name = 'shaun';
+
+console.log(name == 'shaun');  //true
+console.log(name == 'Shaun');
+console.log(name > 'crystal');
+
+
+
+
+//loose and strict comparisons
+
+let age = 25;
+
+// loose comparison (different types can still be equal)
+
+console.log(age == 25);
+console.log(age == '25');
+console.log(age != 25);
+console.log(age != '25');
+
+// strict comparison (different types cannot be equal)
+
+console.log(age === 25);
+console.log(age === '25');
+console.log(age !== 25);
+console.log(age !== '25');
