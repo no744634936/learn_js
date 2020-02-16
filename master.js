ab980bc7f75c699f327c42c72a00cad2a140dc2a
no744634936 <zhanghaifeng1123@gmail.com>
2020-02-16 23:37:19 +0900

    first commit

---
+++ master.js
@@ -0,0 +1,115 @@
+// object literals
+
+let user = {
+    name: 'crystal',
+    age: 30,
+    email: 'crystal@thenetninja.co.uk',
+    location: 'berlin',
+    blogs: ['why mac & cheese rules', '10 things to make with marmite']
+  };
+  //access
+  console.log(user);
+  console.log(user.age);
+  
+  //update
+  user.age = 35;
+  console.log(user.age);
+  
+
+  console.log(user['name']);
+  user['name'] = 'chun-li';
+  console.log(user['name']);
+
+  
+  console.log(typeof user);
+
+
+
+  //methods
+
+  let user = {
+    name: 'crystal',
+    age: 30,
+    email: 'crystal@thenetninja.co.uk',
+    location: 'berlin',
+    blogs: ['why mac & cheese rules', '10 things to make with marmite'],
+    login: function(){
+      console.log('the user logged in');
+    },
+    logout: function(){
+      console.log('the user logged out');
+    },
+    logBlogs: function(){
+      // access the blogs here
+    }
+  };
+  
+  user.login();
+  user.logout();
+
+
+  //this
+  let user = {
+    name: 'crystal',
+    age: 30,
+    email: 'crystal@thenetninja.co.uk',
+    location: 'berlin',
+    blogs: ['why mac & cheese rules', '10 things to make with marmite'],
+    login(){
+      console.log('the user logged in');
+    },
+    logout(){
+      console.log('the user logged out');
+    },
+    logBlogs(){
+      // access the blogs here
+      // console.log(this);
+      // 这里的logBlogs函数不能写成箭头函数，因为箭头函数里的this是指向window的而不是指向user。
+      console.log('this user has written these blogs:');
+      this.blogs.forEach(blog => {
+        console.log(blog);
+      })
+    }
+  };
+  
+  // console.log(this);
+  user.logBlogs();
+
+
+
+//bojects in array
+
+// const blogs = [
+//   {title: 'why mac & cheese rules', likes: 30},
+//   {title: '10 things to make with marmite', likes: 50}
+// ];
+
+// console.log(blogs[0].title);
+
+let user = {
+    name: 'crystal',
+    age: 30,
+    email: 'crystal@thenetninja.co.uk',
+    location: 'berlin',
+    blogs: [
+      {title: 'why mac & cheese rules', likes: 30},
+      {title: '10 things to make with marmite', likes: 50}
+    ],
+    login(){
+      console.log('the user logged in');
+    },
+    logout(){
+      console.log('the user logged out');
+    },
+    logBlogs(){
+      // access the blogs here
+      // console.log(this);
+      console.log('this user has written these blogs:');
+      this.blogs.forEach(blog => {
+        console.log(`${blog.title} has ${blog.likes} likes`);
+      })
+    }
+  };
+  
+  // console.log(this);
+  user.logBlogs();
