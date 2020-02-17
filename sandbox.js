b4cf7418a0ec7bffa2f5465d158b1009d2eb1fd1
no744634936 <zhanghaifeng1123@gmail.com>
2020-02-17 14:15:15 +0900

    second commit

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,4 @@
+//最有用的两个方法。 document.querySelector   and   document.querySelectAll
 // query single element
 //const para = document.querySelector('p');
 // const para = document.querySelector('.error');
@@ -7,6 +8,7 @@ const para = document.querySelector('div.error');
 console.log(para);
 
 // query multiple elements at once
+//return nodeList .can use forEach.
 const paras = document.querySelectorAll('p');
 const errors = document.querySelectorAll('.error');
 
@@ -19,3 +21,27 @@ paras.forEach(para=>{
     console.log(para);
     
 })
+
+
+//chrome -> console -> select element -> copy ->copy selector 就可以取得一个独特的seletor 标识了。
+
+
+
+
+//其他的方法来获得 element
+
+// get an element by ID
+const title= document.getElementById('page-title');
+console.log(title);
+
+// get elements by their class name
+//return html collection and can not use forEach to loop out elements
+const errors = document.getElementsByClassName('error');
+console.log(errors);
+console.log(errors[0]);
+
+// get elements by their tag name
+//return html collection and can not use forEach to loop out elements
+const paras = document.getElementsByTagName('p');
+console.log(paras);
+console.log(paras[1]);
