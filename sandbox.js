4e2bee88ca624e4d005f9a9947dca985e828f661
no744634936 <zhanghaifeng1123@gmail.com>
2020-02-17 13:33:30 +0900

    first commit

---
+++ sandbox.js
@@ -0,0 +1,21 @@
+// query single element
+//const para = document.querySelector('p');
+// const para = document.querySelector('.error');
+// const para = document.querySelector('#img');
+const para = document.querySelector('div.error');
+
+console.log(para);
+
+// query multiple elements at once
+const paras = document.querySelectorAll('p');
+const errors = document.querySelectorAll('.error');
+
+console.log(paras, errors);
+console.log(paras[1], errors[0]);
+
+
+//use forEach function
+paras.forEach(para=>{
+    console.log(para);
+    
+})
