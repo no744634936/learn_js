5591640c543d7ee015ad0b9bc3e8cf638171ca7d
no744634936 <zhanghaifeng1123@gmail.com>
2020-02-18 19:13:19 +0900

    second commit

--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,14 @@
 const ul = document.querySelector('ul');
-// ul.remove();  //删除ul
+// ul.remove();
 
 const button = document.querySelector('button');
 
 button.addEventListener('click', () => {
- //create a new html tag
+  //新建一个html元素
   const li = document.createElement('li');
   li.textContent = 'something new to do';
-  //ul.append(li);    底部添加
-  ul.prepend(li);     //头部添加
+  //ul.append(li); //底部添加
+  ul.prepend(li);
 });
 
 const items = document.querySelectorAll('li');
@@ -16,14 +16,22 @@ const items = document.querySelectorAll('li');
 items.forEach(item => {
   item.addEventListener('click', e => {
     // e.target.style.textDecoration = 'line-through';
-    //点击哪个，哪个就消失
+    //点击那个元素那个元素就消失
     e.target.remove();
   });
 });
 
+//
 
 
 
-//注意这里有个问题，当我点击项目的时候，项目会删除，
-//当我点击按钮的时候，会有新的项目被添加，
-//但是当我点击新添加的项目的时候，新项目不会被删除。
+//when an event occurs like a click that element becomes the event target.(e.target)
+//when this happens javascript will look to see if we have any event listeners attached to this element. 
+//if have listener ,it will fire that callback function for that element 
+//javacript 之后还会去检查，e.target的父节点是否拥有event listener。这里可以看到li 的父节点是ul。如果有就会触发listener里面的callback function 
+//然后 javascript 又会去检查 ul的父节点是否有 event listener，这样一级一级的往上找。 
+
+
+
+
+
