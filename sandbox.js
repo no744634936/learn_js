a974a9a83d566c5bf5dea4fd5ae5a6b81eb79f16
no744634936 <zhanghaifeng1123@gmail.com>
2020-02-19 23:01:18 +0900

    first commit

--- sandbox.js
+++ sandbox.js
@@ -1,16 +1,5 @@
-//when an event occurs like a click that element becomes the event target.(e.target)
-//when this happens javascript will look to see if we have any event listeners attached to this element.
-//if have listener ,it will fire that callback function for that element
-//javacript 之后还会去检查，e.target的父节点是否拥有event listener。这里可以看到li 的父节点是ul。如果有就会触发listener里面的callback function
-
-//然后 javascript 又会去检查 ul的父节点是否有 event listener，这样一级一级的往上找。
-
-//when an event occurs like a click that element becomes the event target.(e.target)
-//when this happens javascript will look to see if we have any event listeners attached to this element. 
-//if have listener ,it will fire that callback function for that element 
-//javacript 之后还会去检查，e.target的父节点是否拥有event listener。这里可以看到li 的父节点是ul。如果有就会触发listener里面的callback function 
-//然后 javascript 又会去检查 ul的父节点是否有 event listener，这样一级一级的往上找。 
-//所以 如果li 有个event 事件，而ul也有event 事件，当li的事件被触发，ul的事件也会被触发。
+//when we click the li ,javascript will bubble up to ul. and excute ul click function
+//我点击ul里面的元素，e.target里面就包含了，这个元素的一些属性，比如tagName=LI
 
 
 const ul = document.querySelector('ul');
@@ -22,23 +11,12 @@ button.addEventListener('click', () => {
   ul.appendChild(li);  
 });
 
-const items = document.querySelectorAll('li');
-
-items.forEach(item => {
-  item.addEventListener('click', e => {
-    console.log('event in LI');
-    //e.stopPropagation(); 在这里加上这句，javascript 就不会buble up的找click事件了。
-    e.stopPropagation();
-    e.target.remove();
-  });
-});
 
 ul.addEventListener('click', e => {
-  console.log('event in UL');
+//   console.log(e.target);
+  console.log(e);
+if (e.target.tagName==="LI"){
+    e.target.remove();
+}
+
 });
-
-
-
-//delegation is useful when we need to attach event listeners to many different elements
-//like these li right here
-//delegation 可以解决 branch 18 的问题
