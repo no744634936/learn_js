0b43708f7e054c5c9bd07d24799feed266469592
no744634936 <zhanghaifeng1123@gmail.com>
2020-02-19 19:00:00 +0900

    fixed

--- sandbox.js
+++ sandbox.js
@@ -21,15 +21,16 @@ items.forEach(item => {
   });
 });
 
-//
+
+//注意这里有一个问题
+//打卡html文件
+//点击项目，项目消失
+//点击按钮，新建项目
+//点击新建的项目，新建的项目不会消失
 
 
 
-//when an event occurs like a click that element becomes the event target.(e.target)
-//when this happens javascript will look to see if we have any event listeners attached to this element. 
-//if have listener ,it will fire that callback function for that element 
-//javacript 之后还会去检查，e.target的父节点是否拥有event listener。这里可以看到li 的父节点是ul。如果有就会触发listener里面的callback function 
-//然后 javascript 又会去检查 ul的父节点是否有 event listener，这样一级一级的往上找。 
+
 
 
 
