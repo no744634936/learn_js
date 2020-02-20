ea13e074fd28292ac2162a760f6b4f89d8cb27f8
no744634936 <zhanghaifeng1123@gmail.com>
2020-02-21 08:48:08 +0900

    first commit

--- sandbox.js
+++ sandbox.js
@@ -1,11 +1,26 @@
 const form = document.querySelector('.signup-form');
-// const username = document.querySelector('#username');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-  // console.log('form submitted');
-  // console.log(username.value);
-
-  //不管是id 还是name是username 都可以这样用。
   console.log(form.username.value);
 });
+
+// testing RegEx
+
+const username = 'shaunyp';
+const pattern = /^[a-z]{6,}$/;
+
+//test return false or true
+// let result = pattern.test(username);
+
+// if(result){
+//   console.log('regex test passed :)');
+// } else {
+//   console.log('regex test failed :(');
+// }
+
+
+//seach return the position.-1 mean do not  get a match .
+let result = username.search(pattern);
+
+console.log(result);
