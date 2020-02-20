dcc06e945e6acff06a8a3d8a0615d23b234a339a
no744634936 <zhanghaifeng1123@gmail.com>
2020-02-20 17:10:52 +0900

    second commit

--- sandbox.js
+++ sandbox.js
@@ -1,5 +1,6 @@
 //when we click the li ,javascript will bubble up to ul. and excute ul click function
 //我点击ul里面的元素，e.target里面就包含了，这个元素的一些属性，比如tagName=LI
+//删除 ul 的li 包括新增的li
 
 
 const ul = document.querySelector('ul');
