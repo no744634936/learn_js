b658cb9f1ca3eac1c4e55030529b9f75b1befdcb
no744634936 <zhanghaifeng1123@gmail.com>
2020-02-21 08:56:01 +0900

    en

--- sandbox.js
+++ sandbox.js
@@ -1,26 +1,12 @@
 const form = document.querySelector('.signup-form');
+// const username = document.querySelector('#username');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
+  // console.log('form submitted');
+  // console.log(username.value);
+
+
+  //username 可以是id 也可以是name 属性
   console.log(form.username.value);
 });
-
-// testing RegEx
-
-const username = 'shaunyp';
-const pattern = /^[a-z]{6,}$/;
-
-//test return false or true
-// let result = pattern.test(username);
-
-// if(result){
-//   console.log('regex test passed :)');
-// } else {
-//   console.log('regex test failed :(');
-// }
-
-
-//seach return the position.-1 mean do not  get a match .
-let result = username.search(pattern);
-
-console.log(result);
