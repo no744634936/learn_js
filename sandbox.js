1625e51eaa9af7c283ac9370744a7a358a3d61eb
no744634936 <zhanghaifeng1123@gmail.com>
2020-02-20 17:30:14 +0900

    first commit

--- sandbox.js
+++ sandbox.js
@@ -1,23 +1,17 @@
-//when we click the li ,javascript will bubble up to ul. and excute ul click function
-//我点击ul里面的元素，e.target里面就包含了，这个元素的一些属性，比如tagName=LI
-//删除 ul 的li 包括新增的li
+const copy = document.querySelector('.copy-me');
 
-
-const ul = document.querySelector('ul');
-const button = document.querySelector('button');
-
-button.addEventListener('click', () => {
-  const li = document.createElement('li');
-  li.textContent = 'something new to do';
-  ul.appendChild(li);  
+copy.addEventListener('copy', () => {
+  console.log('OI! my content is copyrighted!!');
 });
 
+const box = document.querySelector('.box');
 
-ul.addEventListener('click', e => {
-//   console.log(e.target);
-  console.log(e);
-if (e.target.tagName==="LI"){
-    e.target.remove();
-}
+box.addEventListener('mousemove', e => {
+  // console.log(e);
+  // console.log(e.offsetX, e.offsetY);
+  box.textContent = `x pos - ${e.offsetX}  y pos - ${e.offsetY}`;
+});
 
+document.addEventListener('wheel', e => {
+  console.log(e.pageX, e.pageY);
 });
