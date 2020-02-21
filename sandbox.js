5f587672fd41bc47dcb0b4537367fa7676a6af13
no744634936 <zhanghaifeng1123@gmail.com>
2020-02-21 15:33:40 +0900

    first commit

--- sandbox.js
+++ sandbox.js
@@ -1,31 +1,4 @@
-//call back function 后面的参数0，是accumulator的初始值。
+const scores = [10, 5, 0, 40, 60, 10, 20, 70];
 
-
-
-
-// const scores = [10, 20, 60, 40, 70, 90, 30];
-
-// const result = scores.reduce((accumulator, current) => {
-//   if(current > 50){
-//     accumulator++;
-//   }
-//   return accumulator;
-// }, 0);
-
-// console.log(result);
-
-const scores = [
-    {player: 'mario', score: 50},
-    {player: 'yoshi', score: 30},
-    {player: 'mario', score: 70},
-    {player: 'crystal', score: 60}
-  ];
-  
-  const marioTotal = scores.reduce((accumulator, current) => {
-    if(current.player === 'mario'){
-      accumulator += current.score;
-    }
-    return accumulator;
-  }, 0);
-  
-  console.log(marioTotal);
+const firstHighScore = scores.find(score => score > 50);
+console.log(firstHighScore);
