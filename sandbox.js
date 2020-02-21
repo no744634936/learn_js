00d2101c90ea0d7cd7b806f63f4e06443f94f948
no744634936 <zhanghaifeng1123@gmail.com>
2020-02-21 14:51:53 +0900

    first commit

--- sandbox.js
+++ sandbox.js
@@ -1,29 +1,26 @@
-const scores=[10,20,30,40,50,5];
+const prices = [20, 10, 30, 25, 15, 40, 80, 5];
 
-//所有元素都在数组中
-// const filteredScores=scores.filter(()=>{
-//     return true;
-// });
+//create a new array base on old one
+// const salePrices = prices.map(price => price / 2);
+// console.log(salePrices);
 
-//所有元素都不在数组中
-// const filteredScores=scores.filter(()=>{
-//     return false;
-// });
+const products = [
+  {name: 'gold star', price: 20},
+  {name: 'mushroom', price: 40},
+  {name: 'green shells', price: 30},
+  {name: 'banana skin', price: 10},
+  {name: 'red shells', price: 50}
+];
 
-//大于二十的在留下来
-const filteredScores=scores.filter((score)=>{
-    return score>20;
+
+//create a new object base on old one
+const saleProducts = products.map(product => {
+  if(product.price > 30){
+    //注意这里return的是一个新的object 元素
+    return {name: product.name, price: product.price / 2}
+  } else {
+    return product;
+  }
 });
 
-console.log(filteredScores);
-
-//例２
-const users = [
-    {name: 'shaun', premium: true},
-    {name: 'yoshi', premium: false},
-    {name: 'mario', premium: false},
-    {name: 'chun-li', premium: true}
-  ];
-  
-  const premiumUsers = users.filter(user => user.premium);
-  console.log(premiumUsers);
+console.log(products, saleProducts);
