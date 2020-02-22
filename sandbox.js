c412f73cac9c0f269a8f1effb00d55f713bbc378
no744634936 <zhanghaifeng1123@gmail.com>
2020-02-22 15:55:39 +0900

    first commit

--- sandbox.js
+++ sandbox.js
@@ -1,19 +1,16 @@
-const products = [
-    {name: 'gold star', price: 30},
-    {name: 'green shell', price: 10},
-    {name: 'red shell', price: 40},
-    {name: 'banana skin', price: 5},
-    {name: 'mushroom', price: 50}
-  ];
-  
-  // const filtered = products.filter(product => product.price > 20);
-  
-  // const promos = filtered.map(product => {
-  //   return `the ${product.name} is ${product.price / 2} pounds`;
-  // });
-  
-  const promos = products
-    .filter(product => product.price > 20)
-    .map(product => `the ${product.name} is ${product.price / 2} pounds`);
-  
-  console.log(promos);
+//starting something now which can be finished later.
+
+//Async 一般是用来获取数据
+
+
+console.log(1);
+console.log(2);
+
+setTimeout(()=>{
+    console.log("callback function fired");
+},2000)
+
+console.log(3);
+console.log(4);
+
+//打印1，2，3，4然后打印callback function fired
