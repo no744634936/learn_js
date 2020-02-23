90b814f77e6755bb65a9b8025ec3b66990f72d18
no744634936 <zhanghaifeng1123@gmail.com>
2020-02-23 11:04:49 +0900

    first commit

--- sandbox.js
+++ sandbox.js
@@ -1,15 +1,20 @@
 const getData=(callback)=>{
     const request=new XMLHttpRequest();
-    request.open("GET","https://jsonplaceholder.typicode.com/todos/1");
+    // request.open("GET","https://jsonplaceholder.typicode.com/todos");
+    //取得不了本地的json。因为cros的缘故。必须在服务器上才能取得。
+    request.open("GET","my_json.json");
     request.send();
     
     request.addEventListener("readystatechange",()=>{
         if(request.readyState===4 && request.status===200){
-            //在主函数里面给callback函数传参数。这种情况下error为undefined
-            callback(request.responseText,undefined);
+            // json is an array ,elements are object
+            //this takes a json string and convert that json string into js object
+            //when we write json ,everything have to be in double quote.number ok.
+            //json里的最后一个元素没有逗号。
+            const data=JSON.parse(request.responseText);
+            callback(data,undefined);
             
         }else if(request.readyState===4){
-            //在主函数里面给callback函数传参数。这种情况下data为undefined
             callback(undefined,"have no data");
             
         }
@@ -17,14 +22,8 @@ const getData=(callback)=>{
     });
 }
 
-console.log(1);
-console.log(2);
 
-
-//callback方法里面有两种情况，获得数据，得到error
-//这是一个异步方法。
 getData((data,error)=>{
-    // console.log(data,error);
     if(error){
         console.log(error);
         
@@ -33,7 +32,4 @@ getData((data,error)=>{
         
     }
 });
-
-console.log(3);
-console.log(4);
 
