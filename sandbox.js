82ec06282dd0767528d67ccae1f96eb1b90a5c03
zhanghaifeng2019 <zhang@rentracks.co.jp>
2020-03-01 22:46:56 +0900

    first commit

--- sandbox.js
+++ sandbox.js
@@ -1,16 +1,13 @@
-// 例如当我新建一个数组的时候，_proto_里面就有所有数组共同的方法。例如forEach，filter，sort。
-//array 里的_proto_ 指向 array prototype。获得了那些方法。
-//在class 里面定义的方法会自动出现在_proto_属性里面
-//用class 把下面这些步骤简化了。
+
+//仅作了解，使用class 继承的时候到底是怎么做到继承的
 
 
-//这是一个constructor
+//User 的构造器跟方法
 function User(username, email){
     this.username = username;
     this.email = email;
   }
   
-
   User.prototype.login = function(){
     console.log(`${this.username} has logged in`);
     return this;
@@ -21,10 +18,26 @@ function User(username, email){
     return this;
   };
   
-  //userOne 跟 userTwo 里的_proto_属性里面会出现login logout 两个功能。
+
+
+
+  // admin
+  function Admin(username, email,title){
+    //相当于super，呼叫user的构造器并把参数传给user个构造器，记得传this
+    User.call(this, username, email);
+    this.title=title;
+  }
+  
+  //继承User prototype的方法
+  Admin.prototype = Object.create(User.prototype);
+  
+  //添加属于adim的方法
+  Admin.prototype.deleteUser = function(user){
+    // delete the user
+  };
+  
   const userOne = new User('ryu', 'ryu@thenetninja.co.uk');
   const userTwo = new User('chun-li', 'chun.li@thenetninja.co.uk');
+  const userThree = new Admin('shaun', 'shaun@thenetninja.co.uk','cto');
   
-  console.log(userOne);
-  
-  userOne.login().logout();
+  console.log(userThree);
