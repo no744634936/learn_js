edc59cfeda01126d168f4a0788602dbfd5c51fdb
zhanghaifeng2019 <zhang@rentracks.co.jp>
2020-03-01 22:17:59 +0900

    first commit

--- sandbox.js
+++ sandbox.js
@@ -1,25 +1,30 @@
-  
-// constructor functions
-// class User {
-//   constructor(username){
-//     this.username = username;
-//   }
-// }
+// 例如当我新建一个数组的时候，_proto_里面就有所有数组共同的方法。例如forEach，filter，sort。
+//array 里的_proto_ 指向 array prototype。获得了那些方法。
+//在class 里面定义的方法会自动出现在_proto_属性里面
+//用class 把下面这些步骤简化了。
 
-//javascript 特有的，比较老的写法。直接建立一个User constructor方法。注意User要大写。
-//然后还可以把login方法也放进去。
-//即用constructor 替代了class
-//这是比较老的写法，这里提到是为了引出 prototype。
+
+//这是一个constructor
 function User(username, email){
     this.username = username;
     this.email = email;
-    this.login = function(){
-      console.log(`${this.username} has logged in`);
-    };
   }
   
+
+  User.prototype.login = function(){
+    console.log(`${this.username} has logged in`);
+    return this;
+  };
+  
+  User.prototype.logout = function(){
+    console.log(`${this.username} has logged out`);
+    return this;
+  };
+  
+  //userOne 跟 userTwo 里的_proto_属性里面会出现login logout 两个功能。
   const userOne = new User('ryu', 'ryu@thenetninja.co.uk');
   const userTwo = new User('chun-li', 'chun.li@thenetninja.co.uk');
   
-  console.log(userOne, userTwo);
-  userOne.login();
+  console.log(userOne);
+  
+  userOne.login().logout();
