36709a2cb687ee3e2f174546a9fc303b926e7426
no744634936 <33306731+no744634936@users.noreply.github.com>
2020-03-14 17:58:18 +0900

    Update chat.js

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,14 +1,11 @@
-<<<<<<< HEAD
 
 //adding new chat documents
 //setting up a real-time listener to get new chats
 //updating the username
 //updating the chat room
-=======
->>>>>>> d022207d85520e7558e2a51f0151901d0cef9527
 
-//第二部将从数据库里获得的数据通过ui显示出来。看app.js 文件
 
+//manage chatroom data
 class Chatroom{
     constructor(room_name,username){
         this.room=room_name;
@@ -17,7 +14,7 @@ class Chatroom{
         this.unsub
     }
 
-
+    //需要话时间的，存储数据都用async方法。他返回的是一个promise
     async addChat(message){
         let now=new Date();
         let chat={
@@ -32,12 +29,16 @@ class Chatroom{
     }
 
     getChats(callback_function){
+        //只监视room等于特定的room的数据。
+        //第一次用orderBy("created_at")的时候会报告错误说，需要建立index，点击错误后面跟的link，建立index即可
         this.unsub=this.chats
             .where("room","==",this.room)
             .orderBy("created_at")
             .onSnapshot(snapshot=>{
             snapshot.docChanges().forEach(change=>{
                 if(change.type==="added"){
+                    //update the ui.这需要ui class里的方法来做。所以我现在要用一个callback function来做。
+                    //传递callback function是为了以后用ui class 里的方法时能更方便一点。
                     callback_function(change.doc.data());
                 }
             });
@@ -49,12 +50,17 @@ class Chatroom{
 
     updateRoom(room){
         this.room=room;
+
+        //更改了room但是并没更改 realtime listener里的 room名称
+        //要做的就是unsubscribe the real time listener.先关闭然后再开启。
+        //if(this.unsub) 的意思是如果this.unsub有值，
+        //因为必须在real time listener有值之后才能 unsubcribe real time listener
+
         if(this.unsub){
             this.unsub();
         }
     }
 }
-
 
 /*
 //在这里可以改变room跟username
