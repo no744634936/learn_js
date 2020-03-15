e149554dbaaa11713ecbbe1ab147b18961e5ff0d
zhanghaifeng2019 <zhang@rentracks.co.jp>
2020-03-15 12:54:27 +0900

    good

--- sandbox.js
+++ sandbox.js
@@ -1,61 +1,103 @@
-// set 跟数组差不多。但是它不允许有重复元素。如果有会删除重复项，只留一个。
-// sets
-const namesArray = ['ryu', 'chun-li', 'ryu', 'shaun'];
-console.log(namesArray);
+const foo={name:"zhang",age:20};
+const bar={name:"wang",age:30};
+const baz={name:"liu",age:40};
 
-// const namesSet = new Set(['ryu', 'chun-li', 'ryu', 'shaun']);
-const namesSet = new Set(namesArray);
-console.log(namesSet);
+//bad 
+console.log(foo);
+console.log(bar);
+console.log(baz);
 
-// const uniqueNames = [...namesSet];qucong
-//数组去掉重复的方法。
-const uniqueNames = [...new Set(namesArray)];
-console.log(uniqueNames);
+//good
+//可以加css style
+console.log("%c my friends","color:orange;font-weight:bold");
+console.log({foo,bar,baz});
 
-const ages = new Set();
-ages.add(20);
-ages.add(25).add(30);
-ages.add(25);
-ages.delete(30)
-
-console.log(ages, ages.size);  //set 没有 length方法但是有size方法
-console.log(ages.has(30), ages.has(20));
-
-ages.clear();
-console.log(ages);
-
-const ninjas = new Set([
-  {name: 'shaun', age: 30},
-  {name: 'crystal', age: 29},
-  {name: 'chun-li', age: 32}
-]);
-
-ninjas.forEach(ninja => {
-  console.log(ninja.name, ninja.age);
-});
+//good
+console.table([foo,bar,baz]);
 
 
-//---------------------------------------------symbol-----------------------
+//------------------------------log time
+//控制器会打印looper的时间
+console.time("looper");
+let i=0;
+while(i<100000){
+    i++
+}
+console.timeEnd("looper");
 
-// no symbol is the same
 
-const symbolOne = Symbol('a generic name');
-const symbolTwo = Symbol('a generic name');
+// stack trace log
+// if you need to know where a console.log originated from
+// let's imagine we have a really important function that deleted items from our database and 
+// we want to make sure that we don't accidentally call this method twice.
+// console.trace("bye bye database"); 会告诉你，deletedData方法定义在36行.在39 和40行被呼叫
+const deletedData=()=>{
+    // some code
+    console.trace("bye bye database");
+}
 
-console.log(symbolOne, typeof symbolOne);
-console.log(symbolOne === symbolTwo);
+deletedData();
+deletedData();
 
-const ninja = {};
 
-//belt 会被覆盖。
-ninja.age = 30;
-ninja['belt'] = 'orange';
-ninja['belt'] = 'black';
+//destructuring
+const cat={
+    name:"bob",
+    age:2,
+    legs:4,
+    food:"meat"
+}
 
-//虽然symbol都叫 a generic name，但是不会被覆盖。
-//so use the symbol as object keys means that all keys are different.
-ninja[symbolOne] = 'ryu';
-ninja[symbolTwo] = 'shaun';
+//bad
+// function feed(animal){
+//      return `feed ${animal.name} ${animal.food}`;
+// }
 
-console.log(ninja);
-console.log(ninja[symbolOne], ninja[symbolTwo]);
+//good
+// function feed({name,food}){
+//      return `feed ${name} ${food}`;
+// }
+
+//or this way 
+function feed(animal){
+    const {name,food}=animal;
+    return `feed ${name} ${food}`;
+}
+
+console.log(feed(cat));
+
+
+
+//template-literals
+const dog={
+    name:"tom",
+    age:7,
+    legs:4,
+    food:"meat"
+}
+
+
+//good
+const {name,legs,food}=dog;
+const biz=`${name} have ${legs} legs like eat ${food}`;
+console.log(biz);
+
+
+
+// advanced template-literals 
+//This dog is 就是 str[0]
+function dogAge(str,age){
+    const ageStr=age>5 ? "old" : "young";
+    return `${str[0]} ${ageStr} at ${age} years`;
+}
+
+
+//dogAge 后面没有括号。
+const bio2= dogAge`This dog is ${dog.age}`;
+console.log(bio2);
+
+
+
+
+//good loop
+//使用 reduce map filter 等方法
