8a669ab0be7b24a9ff82e8b6a2f154027c84c3ff
zhanghaifeng2019 <zhang@rentracks.co.jp>
2020-03-15 11:24:07 +0900

    good

--- sandbox.js
+++ sandbox.js
@@ -1,22 +1,61 @@
-// rest parameter
-const double = (...nums) => {
-    console.log(nums);
-    return nums.map(num => num*2);
-  };
-  
-  const result = double(1,3,5,7,2,4,6,8);
-  console.log(result);
-  
-  // spread syntax (arrays)
-  const people = ['shaun', 'ryu', 'chun-li'];
-  const members = ['mario', 'luigi', ...people];
-  console.log(members);
-  
-  // spread syntax (objects)
-  // const personClone = person 并不能复制一个object。要这样
-  // const personClone = { ...person};
-  const person = { name: 'shaun', age: 30, job: 'net ninja' };
+// set 跟数组差不多。但是它不允许有重复元素。如果有会删除重复项，只留一个。
+// sets
+const namesArray = ['ryu', 'chun-li', 'ryu', 'shaun'];
+console.log(namesArray);
+
+// const namesSet = new Set(['ryu', 'chun-li', 'ryu', 'shaun']);
+const namesSet = new Set(namesArray);
+console.log(namesSet);
+
+// const uniqueNames = [...namesSet];qucong
+//数组去掉重复的方法。
+const uniqueNames = [...new Set(namesArray)];
+console.log(uniqueNames);
+
+const ages = new Set();
+ages.add(20);
+ages.add(25).add(30);
+ages.add(25);
+ages.delete(30)
+
+console.log(ages, ages.size);  //set 没有 length方法但是有size方法
+console.log(ages.has(30), ages.has(20));
+
+ages.clear();
+console.log(ages);
+
+const ninjas = new Set([
+  {name: 'shaun', age: 30},
+  {name: 'crystal', age: 29},
+  {name: 'chun-li', age: 32}
+]);
+
+ninjas.forEach(ninja => {
+  console.log(ninja.name, ninja.age);
+});
 
 
-  const personClone = { ...person, location: 'manchester' };
-  console.log(person, personClone);
+//---------------------------------------------symbol-----------------------
+
+// no symbol is the same
+
+const symbolOne = Symbol('a generic name');
+const symbolTwo = Symbol('a generic name');
+
+console.log(symbolOne, typeof symbolOne);
+console.log(symbolOne === symbolTwo);
+
+const ninja = {};
+
+//belt 会被覆盖。
+ninja.age = 30;
+ninja['belt'] = 'orange';
+ninja['belt'] = 'black';
+
+//虽然symbol都叫 a generic name，但是不会被覆盖。
+//so use the symbol as object keys means that all keys are different.
+ninja[symbolOne] = 'ryu';
+ninja[symbolTwo] = 'shaun';
+
+console.log(ninja);
+console.log(ninja[symbolOne], ninja[symbolTwo]);
