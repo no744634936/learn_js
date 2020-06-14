1b9e1c8f9e30f95b6da6ad43e80344260df0797d
zhanghaifeng2019 <zhang@rentracks.co.jp>
2020-06-14 22:47:36 +0900

    good

--- 1.js
+++ 1.js
@@ -1,27 +1,40 @@
 
-let wizard={
-    name:"Merlin",
-    health:50,
-    // heal:()=>{
-    //     console.log(this.health);  //undefined
-    //     this.health=100;           //object里不能使用箭头函数，因为箭头函数里的this没用
-    // },
+//数组也是object ，数组参照类型。所以改动一个另一个也会受影响
+var c = [1,2,3];
+var d = c;
+d.push( 4 );
 
-    // heal=()=>{                    //这种写法也不对
-    //     this.health=100; 
-    // },
-
-    heal(){                         //只有这种写法是对的
-        this.health=100; 
-    }
-}
+console.log(c);   // [1,2,3,4]
+console.log(d);   // [1,2,3,4]
 
 
 
-wizard.heal();
-console.log(wizard.health);
+//那么要如何复制一个数组呢？要这样写
+let a=[1,2,3];
+let b=[].concat(a);
+b.push(4);
+
+console.log(a);    //[ 1, 2, 3 ]
+console.log(b);   //[ 1, 2, 3, 4 ]
 
 
+
+//object 的时候。要如何复制object呢
+let obj = {
+    a: 'a',
+    b: 'b',
+    c: "c"
+};
+
+
+let clone = Object.assign({}, obj);
+let clone2 = {...obj}
+
+obj.c=5;
+
+console.log(obj);    //{ a: 'a', b: 'b', c: 5 }    改变了
+console.log(clone);  //{ a: 'a', b: 'b', c: 'c' }  没改变
+console.log(clone2); //{ a: 'a', b: 'b', c: 'c' }  没改变
 
 
 
--- 2.js
+++ 2.js
@@ -1,15 +1,18 @@
+//object 深层克隆
 
-//这里有一个巨大大坑，以后写代码一定要注意。
-
-let obj={
-    name:"zhanghaifeng",
-    sing(){
-        console.log("a",this);        //this 指向obj
-        let anotherfunc=function(){  
-            console.log("b",this);   //this 在浏览器运行的话指向window，在node.js里运行的指向global object
-        }
-        anotherfunc();
+let obj = {
+    a: 'a',
+    b: 'b',
+    c: {
+      deep: 'try and copy me'
     }
-}
-
-obj.sing()
+};
+  let clone = Object.assign({}, obj);    
+  let clone2 = {...obj}
+  let superClone = JSON.parse(JSON.stringify(obj))    //先将obj转换成字符串，然后将字符串转换成object，再赋值。
+  
+  obj.c.deep = 'hahaha';
+  console.log(obj)             //{ a: 'a', b: 'b', c: { deep: 'hahaha' } }
+  console.log(clone)           //没有克隆成功受到影响        { a: 'a', b: 'b', c: { deep: 'hahaha' } }
+  console.log(clone2)          //没有克隆成功受到影响        { a: 'a', b: 'b', c: { deep: 'hahaha' } }
+  console.log(superClone)     // 克隆成功 没有收到修改的影响  { a: 'a', b: 'b', c: { deep: 'try and copy me' } }
