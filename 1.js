25a2b1ef13cc8ca262fef14b9a84aee34acc8bd0
zhanghaifeng2019 <zhang@rentracks.co.jp>
2020-06-14 23:41:18 +0900

    good

---
+++ 1.js
@@ -0,0 +1,28 @@
+//在javascrpt中，function 可以
+
+const { loadavg } = require("os");
+const { log } = require("util");
+
+//1，被赋值给一个变量
+
+let stuff=()=>{}
+
+//2，可以被当作参数传递
+let a=(fn)=>{
+    fn();
+}
+
+a(()=>{console.log("haha")})
+
+
+//可以被return
+
+b=()=>{
+    return c=()=>{console.log("bbbb");
+    }
+}
+
+// b()();
+
+let d=b();
+d();
